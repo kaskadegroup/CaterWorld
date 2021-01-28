08ddd42e8a4b96b8ffc1baf9dae4fa5c5683f9ae
Arjun Sachdeva <sachdevaarjun29@gmail.com>
2021-01-27 22:34:02 -0500

    name changes

--- android/app/src/main/kotlin/com/kaskade/toastea/MainActivity.kt
+++ android/app/src/main/kotlin/com/kaskade/toastea/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.caterWorld
+package com.kaskade.toastea
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -5,7 +5,7 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
           body: Center(
-        child: Text("CaterWorld"),
+        child: Text("toastea"),
       )
     );
   }
