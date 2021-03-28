37a0cf3754b3b3625ab61dcb45af1da250099fee
sarthakbabbar <sarthakbabbar.07@gmail.com>
2021-03-28 13:57:28 -0400

    basic working web approval proccess

--- api/app.py
+++ api/app.py
@@ -1,28 +1,39 @@
 import os
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for, Response
 from firebase_admin import credentials, firestore, initialize_app
 
 app = Flask(__name__)
-print(os.path.dirname(os.path.abspath(__file__)))
 cred = credentials.Certificate('../../../secret/tostea/key.json')
 defualt_app = initialize_app(cred)
 db = firestore.client()
 dishes_ref = db.collection('dishInfo')
 
-@app.route('/dishes/all_dishes', methods=['GET'])
+
+@app.route('/dishes/all_dishes', methods=['GET', 'POST'])
 def dishes():
-    dishes = [doc.to_dict() for doc in dishes_ref.stream()]
-    print(dishes[0])
-    return render_template('dishes.html', dishes=dishes)
+    dishes = [doc.to_dict() for doc in dishes_ref.where("status", '==', 'PENDING').stream()]
+    return render_template('all_dishes.html', dishes=dishes)
 
 
-@app.route('/dishes/<dish_id>')
+@app.route('/dishes/<dish_id>', methods=['GET', 'POST'])
 def get_dish(dish_id):
-    dish = dishes_ref.document(dish_id).get()
-    return jsonify(dish.to_dict()), 200
+
+    dish = dishes_ref.document(dish_id).get().to_dict()
+
+    return render_template('dish.html', dish=dish)
+
+
+@app.route('/update', methods=['POST', 'PUT'])
+def update():
+    try:
+        id = request.form['id']
+        dishes_ref.document(id).update({'status':'APPROVED'})
+        return redirect(url_for('dishes'))
+    except Exception as e:
+        return f"An Error Occured: {e}"
 
 
 port = int(os.environ.get('PORT', 8080))
 if __name__ == '__main__':
 
-    app.run(threaded=True, host='0.0.0.0', port=port)
+    app.run(threaded=True, host='0.0.0.0', port=port, debug=True)
